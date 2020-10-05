1ca7865780172ad195afb482d98b5b4b30f103cc
eswaters <eswaters@asu.edu>
2020-10-04 18:28:25 -0700

    initial commit

---
+++ AddingMachine.java
@@ -0,0 +1,36 @@
+package cse360assignment02;
+/**
+ * This function creates an AddingMachine object that keeps a history of
+ * the functions that you call. Calling System.out.println() on an
+ * object of type AddingMachine will print its history.
+ */
+public class AddingMachine {
+  private int total;
+  
+  //constructor
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  //returns total
+  public int getTotal () {
+    return 0;
+  }
+  
+  //adds value
+  public void add (int value) {
+  }
+
+  //subtracts value
+  public void subtract (int value) {
+  }
+
+  //print history
+  public String toString () {
+    return "";
+  }
+
+  //clear history
+  public void clear() {
+  }
+}
