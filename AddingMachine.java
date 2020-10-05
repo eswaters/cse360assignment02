daffaf10070c3937e2188a6dc5864ff0a3e3a1ba
eswaters <eswaters@asu.edu>
2020-10-04 18:28:45 -0700

    final commit

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,10 +1,25 @@
 package cse360assignment02;
+import java.util.*;
 /**
  * This function creates an AddingMachine object that keeps a history of
  * the functions that you call. Calling System.out.println() on an
  * object of type AddingMachine will print its history.
  */
 public class AddingMachine {
+
+  public static void main(String[] args)
+  {
+    AddingMachine myCalculator = new AddingMachine();
+    myCalculator.add (4); 
+    myCalculator.subtract (2); 
+    myCalculator.add(5);
+    System.out.println(myCalculator);
+    System.out.println(myCalculator.getTotal());
+    myCalculator.clear();
+    System.out.println(myCalculator);
+  }
+
+  private String totalString = "0 ";
   private int total;
   
   //constructor
@@ -14,23 +29,28 @@ public class AddingMachine {
   
   //returns total
   public int getTotal () {
-    return 0;
+    return this.total;
   }
   
   //adds value
   public void add (int value) {
+    totalString += "+ " + Integer.toString(value) + " ";
+    this.total += value;
   }
 
   //subtracts value
   public void subtract (int value) {
+    totalString += "- " + Integer.toString(value) + " ";
+    this.total -= value;
   }
 
   //print history
   public String toString () {
-    return "";
+    return totalString;
   }
 
   //clear history
   public void clear() {
+    totalString = "0 ";
   }
 }
